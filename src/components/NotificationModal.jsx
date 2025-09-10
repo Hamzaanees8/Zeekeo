fcb1cf2348b1c24e33cbc2f15cc66c4e8685193d
hamzaanees8 <hamza.anees.432@gmail.com>
2025-09-10 16:11:49 +0500

    Updated Notification and added rounded corners

--- src/components/NotificationModal.jsx
+++ src/components/NotificationModal.jsx
@@ -20,8 +20,11 @@ export default function NotificationModal({ onClose }) {
   ];
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center " style={{ backgroundColor: "rgba(69, 69, 69, 0.4)" }}>
-      <div className="bg-white w-[450px] px-6 py-[8px] font-urbanist text-[#7E7E7E] font-normal">
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center "
+      style={{ backgroundColor: "rgba(69, 69, 69, 0.4)" }}
+    >
+      <div className="bg-white w-[450px] px-6 py-[8px] font-urbanist text-[#7E7E7E] font-normal rounded-[8px] shadow-md">
         {/* Header */}
         <div className="flex items-center justify-between border-b border-b-[#6D6D6D] py-2">
           <h2 className="text-[20px] font-semibold text-[#04479C] flex items-center gap-2">
@@ -34,7 +37,7 @@ export default function NotificationModal({ onClose }) {
 
         {/* Notifications List */}
         <div className="flex-1 overflow-y-auto pt-1 pb-2 custom-scroll">
-          {notifications.map((n) => (
+          {notifications.map(n => (
             <div
               key={n.id}
               className="border-b py-3 flex flex-col gap-1 last:border-none"
@@ -45,7 +48,9 @@ export default function NotificationModal({ onClose }) {
                 </span>
                 <span className="font-[600] text-[#7E7E7E]">{n.title}</span>
               </div>
-              <p className="text-[#7E7E7E] text-[14px] leading-[130%]">{n.text}</p>
+              <p className="text-[#7E7E7E] text-[14px] leading-[130%]">
+                {n.text}
+              </p>
             </div>
           ))}
         </div>
