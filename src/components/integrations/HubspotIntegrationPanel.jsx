45e1566f2d6c864ca0fe841f7b26d2e56501ea9c
hamzaanees8 <hamza.anees.432@gmail.com>
2025-10-25 01:37:16 +0500

    Font removed

--- src/components/integrations/HubspotIntegrationPanel.jsx
+++ src/components/integrations/HubspotIntegrationPanel.jsx
@@ -7,7 +7,7 @@ const HubspotIntegrationPanel = ({ onClose }) => {
   const [activeTab, setActiveTab] = useState("contacts");
 
   const tabs = [
-  //  { key: "custom-field-settings", label: "Custom Field Settings" },
+    //  { key: "custom-field-settings", label: "Custom Field Settings" },
     { key: "contacts", label: "Existing Contacts" },
     { key: "settings", label: "Export to HubSpot Settings" },
   ];
@@ -15,10 +15,15 @@ const HubspotIntegrationPanel = ({ onClose }) => {
   return (
     <div className="bg-white border border-gray-200 rounded-xl p-6 shadow-md w-full min-h-[600px]">
       <div className="flex justify-between items-center border-b pb-3 mb-4">
-        <h2 className="text-[#7E7E7E] font-[500] font-urbanist text-[20px] font-semibold ">
+        <h2 className="text-[#7E7E7E] font-[500] font-urbanist text-[20px]">
           HubSpot Configuration
         </h2>
-        <button variant="outline" size="sm" onClick={onClose} className="cursor-pointer text-[#7E7E7E] font-[500] ">
+        <button
+          variant="outline"
+          size="sm"
+          onClick={onClose}
+          className="cursor-pointer text-[#7E7E7E] font-[500] "
+        >
           X
         </button>
       </div>
